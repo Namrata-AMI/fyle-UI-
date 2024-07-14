76624928e641f33c4bb0d04eb7dda73aec77767f
Namrata <namratateshwer@gmail.com>
2024-07-14 18:24:13 +0530

    added new repo

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get("/app",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.post("/app",(req,res)=>{
+    const {email,firstName,LastName} = req.body;
+    console.log(req.body);
+});
+
+app.get("/app/service",(req,res)=>{
+    res.render("service.ejs");
+});
+
+
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+})
+
